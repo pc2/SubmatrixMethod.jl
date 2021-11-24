06363a3911d180556281f19891f1e1819c1f6479
Documenter.jl <documenter@juliadocs.github.io>
2021-11-24 16:02:17 +0100

    build based on b953034

--- dev/examples/matrix_inv.jl
+++ dev/examples/matrix_inv.jl
@@ -7,6 +7,7 @@ SubmatrixMethod.disable_benchmarks() # hide
 
 # A sparse symmetric input matrix
 M = SubmatrixMethod.generate_input_matrix(1000, 0.001)
+# M = SubmatrixMethod.generate_input_matrix(10_000, 0.0001)
 
 # The operation that we want to consider is a matrix inversion.
 # However, directly calling `inv` doesn't work
@@ -26,12 +27,15 @@ Mdense = Matrix(M);
 
 # Submatrix for sparse input
 @btime submatrix_apply($inv, $M; multithreading=false);
+@btime submatrix_apply($sqrt, $M; multithreading=false);
 
 # We can speed up things further by enabling multithreading
 @btime submatrix_apply($inv, $M; multithreading=true);
+@btime submatrix_apply($sqrt, $M; multithreading=true);
 
 # Other multithreading
 @btime submatrix_apply($inv, $M; multithreading=:threads);
+@btime submatrix_apply($sqrt, $M; multithreading=:threads);
 
 # Make sure that Julia is actually running with multiple threads
 Threads.nthreads()
