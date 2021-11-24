14b7bee1c959ee43e8088da645ce6691cf3a50db
Documenter.jl <documenter@juliadocs.github.io>
2021-11-24 19:00:48 +0100

    build based on 03e505a

--- dev/examples/matrix_inv.jl
+++ dev/examples/matrix_inv.jl
@@ -1,38 +1,65 @@
 # # Speeding up Matrix Inversion
 
-# Load the packages
+# ## Regular inversion
+# First, we need to generate a random input matrix `M` that we want to invert.
+# However, since the submatrix method expects a sparse symmetric
+# positive definite matrix, we can't just use `rand`. Instead,
+# we use the utility function [`SubmatrixMethod.generate_input_matrix`](@ref) to
+# generate our input matrix.
 using SubmatrixMethod
-using BenchmarkTools
 SubmatrixMethod.disable_benchmarks() # hide
-
-# A sparse symmetric input matrix
 M = SubmatrixMethod.generate_input_matrix(1000, 0.001)
-# M = SubmatrixMethod.generate_input_matrix(10_000, 0.0001)
 
-# The operation that we want to consider is a matrix inversion.
-# However, directly calling `inv` doesn't work. Since the inverse of
-# a sparse matrix is generally dense, we need to convert `M` to a
-# dense matrix first.
+# Note that `M` isn't just sparse in the value sense but actually
+# a `SparseMatrixCSC` datastructure.
+typeof(M)
+
+# If we try to naively invert this matrix, i.e. calling `inv(M)`, Julia will throw an
+# error, reminding us of the fact that the inverse of a sparse matrix is generally dense.
+# To circumvent this error, we first need to convert `M` to a dense `Matrix{Float64}` first.
 Mdense = Matrix(M);
 
-# Establishing a baseline:
+# Now, `inv(M)` does what it's supposed to do.
+using LinearAlgebra, Test
+@test inv(Mdense) * Mdense ≈ I
+
+# Alright, let's load [BenchmarkTools.jl](https://github.com/JuliaCI/BenchmarkTools.jl) and
+# benchmark how long the conventional inversion takes.
+# This will serve as a baseline that we can compare to the submatrix method.
+using BenchmarkTools
 @btime inv($Mdense);
 
-# Submatrix for dense input
+# ## Submatrix method
+
+# Using the function `submatrix_apply` by SubmatrixMethod.jl, we can get a (good) approximation of `inv(Mdense)` via `submatrix_apply(inv, Mdense)`.
+M̃inv = submatrix_apply(inv, Mdense)
+Minv = inv(Mdense)
+maximum(abs.(M̃inv .- Minv))
+
+# However, note that the computation based on the submatrix method is much faster
 @btime submatrix_apply($inv, $Mdense);
 
-# Submatrix for sparse input
+# And it is even faster if we provide the sparse matrix (`SparseMatrixCSC`) directly
 @btime submatrix_apply($inv, $M);
 
-# We can speed up things further by enabling multithreading.
-# Of course, this only works if we've started Julia with multiple threads.
+# ## Multithreading
+
+# Depending on the available resources (and the matrix size/sparsity etc.) we can try
+# to speed things up further by enabling the multithreading functionality of SubmatrixMethod.jl.
+# Of course, this only works if we've started Julia with multiple threads in the first place.
 Threads.nthreads()
 
-# Also, to avoid conflicts with BLAS's built-in multithreading, we should
-# set the number of BLAS threads to one.
+# To avoid conflicts with BLAS's built-in multithreading, it is generally recommended
+# to set the number of BLAS threads to one.
 BLAS.set_num_threads(1)
 
-# Let's now benchmark the multithreaded variant
+# Alright, here comes the benchmark of the multithreaded variant
 @btime submatrix_apply($inv, $M; multithreading=true);
 
-# Hope this was helpful!
+# ## Scaling
+#
+# * [As a function of the size of the input matrix (and for various densities)](https://git.uni-paderborn.de/pc2/julia/submatrixmethod.jl/-/tree/master/analysis/scaling_matrixsize)
+# * [As a function of the number of Julia / BLAS threads](https://git.uni-paderborn.de/pc2/julia/submatrixmethod.jl/-/tree/master/analysis/scaling_multithreading)
+
+# So much about speeding up matrix inversion with the submatrix method. See you in the next tutorial!
+
