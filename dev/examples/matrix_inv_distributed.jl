63b8457de4e2ddb5dde39345973f121476078642
Documenter.jl <documenter@juliadocs.github.io>
2021-12-20 12:00:10 +0100

    build based on d23bbb8

--- dev/examples/matrix_inv_distributed.jl
+++ dev/examples/matrix_inv_distributed.jl
@@ -47,6 +47,20 @@ maximum(abs.(Ainv .- inv(Matrix(A))))
 # @btime submatrix_apply($inv, $A, $(DistributedSerial()));
 # ```
 
+# ### `HDF5Input`
+#
+# You can also use [`HDF5Input`](@ref) to make `submatrix_apply` use a sparse matrix stored
+# in a HDF5 file (should have been stored with [`write_hdf5`](@ref)).
+# In this case, the input matrix isn't send to workers but instead read / mmapped from disk
+# on every worker. This lowers the inter-worker communication at the cost of (parallel)
+# disk accesses.
+# ```julia
+# # if an A::SparseMatrixCSC is provided to the HDF5Input constructor
+# # the matrix is written to file before the HDF5Input wrapper is created
+# Ah5 = HDF5Input(A; fname="A.h5", path="A", mmap=mmap, overwrite=true)
+# R = submatrix_apply(inv, Ah5, DistributedSerial())
+# ```
+
 # That's it.
 # ```julia
 # nworkers() > 1 && rmprocs(workers()) # hide
